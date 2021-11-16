470f1fd79dc43eeb27274c4019b09dcae1890cc3
Sterberino <91395511+Sterberino@users.noreply.github.com>
2021-11-16 03:39:56 -0800

    Minor Additions
    Added:
    -Game Manager class (singleton design pattern)
    -Skeleton for Melee attack behavior

---
+++ CS100-final-project/Assets/Scripts/CharacterMeleeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMeleeScript : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+    private bool attacking;
+
+
+    public KeyCode attackKeycode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(anim == null)
+        {
+            anim = GetComponent<Animator>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(attackKeycode) && !attacking)
+        {
+            StartCoroutine(MeleeAttack());
+        }
+    }
+
+    public IEnumerator MeleeAttack()
+    {
+
+
+        yield return null;
+    }
+}
---
+++ CS100-final-project/Assets/Scripts/PersistentGameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Persistent Game Manager handles game states, such as whether the game is paused, which must be easily accessed by many if not all game objects. 
+/// </summary>
+public class PersistentGameManager : MonoBehaviour
+{
+    public static PersistentGameManager Instance;
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            //DontDestroyOnLoad means that if the scene in which this game object is located in initially gets unloaded, this 
+            //game object continues existing.
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public bool Paused()
+    {
+        return isPaused;
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+    }
+
+    public void UnPauseGame()
+    {
+        isPaused = false;
+    }
+
+}
