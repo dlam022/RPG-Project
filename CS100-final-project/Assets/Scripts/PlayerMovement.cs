642d806ebb205d6b69308ab463d43a13b3757ca1
Sterberino <91395511+Sterberino@users.noreply.github.com>
2021-11-16 03:10:45 -0800

    Large Commit
    Added:
    -Game World
    -Character Controller Script
    -Tile palette
    -Art assets with proper import settings

---
+++ CS100-final-project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 movement = Vector3.zero;
+    public float movementSpeed;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
+
+        if(movement != Vector3.zero)
+        {
+            anim.SetBool("Is Moving", true);
+            anim.SetFloat("Move X", movement.x);
+            anim.SetFloat("Move Y", movement.y);
+            transform.position += movement * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            anim.SetBool("Is Moving", false);
+        }
+
+        
+    }
+
+  
+}
